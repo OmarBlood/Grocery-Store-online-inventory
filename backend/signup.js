fe0b6827c94e4194e96152a53a62f9c8cacd8985
SomePenguin <arandomizedpenguin@gmail.com>
2019-03-12 22:30:27 -0230

    Created functions for server interaction

--- backend/signup.js
+++ backend/signup.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const format = require('string-format');
 const cookieSession = require('cookie-session');
-
 const db = new sqlite3.Database( __dirname + '/users.db',
 	function(err){
 		if(!err){
@@ -12,8 +11,8 @@ const db = new sqlite3.Database( __dirname + '/users.db',
 				id INTEGER PRIMARY KEY,
 				firstName TEXT,
 				lastName TEXT,
-				username UNIQUE TEXT,
-				password UNIQUE TEXT,
+				username TEXT,
+				password TEXT,
 				email TEXT,
 				dob DATE
 			)`);
@@ -28,55 +27,85 @@ app.use(express.static( __dirname ));
 app.use(bodyParser.urlencoded({extended:true}));
 //Still need to figure out how to incorporate cookies
 app.use(cookieSession({
-	name: 'session'
+	name: 'session',
 	secret: 'foo'
 }));
 
-app.post('/signup.html', function(req,res) {
-	//Right now, this only checks if the input fields aren't blank
-	//When we have a database, we can just change the condition to...
-	//...check whether the input matches existing database info.
+function check_blank_signup(req,res){
 	if(!req.body.firstName){
-		res.redirect('/signup.html');
+		res.redirect('/signup');
 		//Need to figure out how to print an error message on screen
 		//Or at least point out which field isn't right
 		console.log('Cannot have a blank first name!');
 	}
 	else if(!req.body.lastName){
-		res.redirect('/signup.html');
+		res.redirect('/signup');
 		console.log('Cannot have a blank last name!');
 	}
 	else if(!req.body.username){
-		res.redirect('/signup.html');
+		res.redirect('/signup');
 		console.log('Cannot have a blank username!');
 	}
 	else if(!req.body.password){
-		res.redirect('/signup.html');
+		res.redirect('/signup');
 		console.log('Cannot have a blank password!');
 	}
 	else if(!req.body.email){
-		res.redirect('/signup.html');
+		res.redirect('/signup');
 		console.log('Cannot have a blank email address!');
 	}
 	else if(!req.body.dob){
-		res.redirect('/signup.html');
+		res.redirect('/signup');
 		console.log('Cannot have a blank date of birth!');
 	}
-	else{
-		db.run(`INSERT INTO users(firstName,lastName,username,password,email,dob) VALUES(?,?,?,?,?,?)`,
-			[req.body.firstName, req.body.lastName, req.body.username, req.body.password, req.body.email, req.body.dob],
-			function(err) { if(!err) {res.redirect('/signup.html'); }}
-		);
-		res.redirect('/login.html');
+}
+
+function check_blank_login(req,res){
+	if(req.body.username){
+		if(req.body.password){
+			res.redirect('/landing');
+		}
+		else{
+			res.redirect('/login');
+		}
 	}
+	else{
+		res.redirect('/login');
+	}
+}
+
+//Canned spaghetti code below...
+//function check_unique(req,res) {
+//	if(req.body.firstName){
+//		db.run(`SELECT DISTINCT firstName FROM users`,[], function(err, fnames) {
+//			if(!err){
+//				for i in fnames{
+//					if(req.body.firstName === fnames[i]){
+//						res.redirect('/signup');
+//						console.log('First name is already registered in database.');
+//					}
+//				}
+//			}
+//		}
+//	}
+//}
+
+app.get('/signup', function(req,res) {
+	console.log("Reached the signup page.");
+	res.redirect('/signup.html');
 });
 
-app.post('/login.html', function(req,res) {
-	//Same issue as the above: only checks blank fields, nothing else.
-	if(req.body.username && req.body.password){
-		res.redirect('/landing.html');
-	}
-	else{
-		res.redirect('/login.html');
-	}
+app.post('/signup.html', check_blank_signup, function(req,res) {
+});
+
+app.get('/login', function(req,res) {
+	console.log("Reached the login page.");
+	res.redirect('/login.html');
+});
+
+app.post('/login.html', check_blank_login, function(req,res) {
+});
+
+app.listen(port, function() {
+    console.log(`Listening on port ${port}!`);
 });
