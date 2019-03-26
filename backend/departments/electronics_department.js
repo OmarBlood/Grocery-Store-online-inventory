7a508ecff04fecfb77aa82c741c0074b04f0761f
SomePenguin <arandomizedpenguin@gmail.com>
2019-03-25 21:42:52 -0230

    Fixed a minor typo

--- backend/departments/electronics_department.js
+++ backend/departments/electronics_department.js
@@ -5,7 +5,7 @@ const sqlite3 = require('sqlite3').verbose();
 const format = require('string-format');
 const cookieSession = require('cookie-session');
 
-const db = new sqlite3.Database( __dirname + '/food_dpt.db', function(err){
+const db = new sqlite3.Database( __dirname + '/electronics_dpt.db', function(err){
 	if(err){
 		console.log(err);
 	}
